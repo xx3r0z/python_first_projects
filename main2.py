87bff2b52b31de371a4708619b101dd66ed345bb
joseph <tobij337@gmail.com>
2025-11-24 17:23:11 +0100

    Implemented Edit button feature and update the UI

---
+++ main2.py
@@ -0,0 +1,4 @@
+names = ['he is a boy', 'she is a girl', 'they are having fun']
+index = names.index('he is a boy')
+
+print(index)
--- todo_app/gui.py
+++ todo_app/gui.py
@@ -4,12 +4,14 @@ import FreeSimpleGUI as fsg
 label = fsg.Text("Type in a todo")
 input_box = fsg.InputText(tooltip="Enter todo", key="todo")
 add_button = fsg.Button("Add")
+list_box = fsg.Listbox(values=functions.open_todos(), key='todosList',
+                       enable_events=True, size=[45, 10])
 edit_button = fsg.Button("Edit")
 complete = fsg.Button("Complete")
 close = fsg.Button("Close App")
 
 window = fsg.Window('My To-do App',
-                    layout=[[label], [input_box], [add_button], [edit_button, complete], [close]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button], [complete], [close]],
                     font=('Helvetica', 20))
 # window.read() returns a tuple containing the event(e.g btn click) and a dictionary containing a key
 # and the value of the key is what is submitted in the input box. The default key starts from 0...
@@ -19,6 +21,8 @@ window = fsg.Window('My To-do App',
 
 while True:
     event, values = window.read()
+    print('Event: ', event)
+    print("Values: ", values)
 
     match event:
         case 'Add':
@@ -27,6 +31,39 @@ while True:
             todos.append(values['todo'] + '\n')
 
             functions.write_todos(todos)
+
+            window['todosList'].update(values=todos)
+
+        case 'Edit':
+            edit_todo = values['todosList'][0]
+            new_todo = values['todo']
+
+            todos = functions.open_todos()
+
+            index = todos.index(edit_todo)
+            print('index: ', index)
+            print('todos: ', todos)
+            print('edit todo: ', edit_todo)
+            todos[index] = new_todo + '\n'
+
+            functions.write_todos(todos)
+
+            window['todosList'].update(values=todos)
+
+        case 'todosList':
+            window['todo'].update(values['todosList'][0].strip('\n'))
+
+        case 'Complete':
+            complete_todo = values['todosList'][0]
+            todos = functions.open_todos()
+            index = todos.index(complete_todo)
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            window['todosList'].update(values=todos)
+
+
         case fsg.WIN_CLOSED:
             break
 
